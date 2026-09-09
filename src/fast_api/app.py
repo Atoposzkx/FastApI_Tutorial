f5a22a8bc29e0b05f0d425c7821b94676d9d7e4e
atoposzkx <atoposzkx@gmail.com>
2026-09-09 23:55:49 +0800

    增加新的注释

--- src/fast_api/app.py
+++ src/fast_api/app.py
@@ -1,8 +1,73 @@
-from fastapi import FastAPI,HTTPException
+'''
+你当前项目实际发生的事情
+运行：
+uv run python -m fast_api.main
+或者通过 Uvicorn 启动时：
+1. Python 导入 fast_api.app。
+2. 导入 db.py，创建 engine 和 Session 工厂。
+3. 创建 FastAPI 对象。
+4. Uvicorn 调用 lifespan。
+5. create_db_and_tables() 根据 Base.metadata 创建 posts 表。
+6. 执行到 yield，FastAPI 开始接收请求。
+7. 当前接口仍然在操作 text_posts 内存字典，还没有真正使用数据库。
+8. 关闭服务器时，从 yield 后面继续执行。
+'''
+from fastapi import FastAPI,HTTPException,File,UploadFile,Form,Depends
 from fast_api.schemas import PostCreate,PostResponse
+from fast_api.db import Post,create_db_and_tables,get_async_session,engine
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
+'''
+我们希望：
 
-app = FastAPI()
+FastAPI 启动
+     ↓
+自动创建数据库表
+     ↓
+服务器开始接收请求
 
+'''
+
+#lifespan 管理整个 FastAPI 应用从启动到关闭的生命周期
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    #启动阶段,await:等待数据库建表完成；等待期间允许事件循环处理其他异步任务。
+    await create_db_and_tables()
+    #启动阶段已经完成，现在把控制权交给 FastAPI；函数暂时不结束，等应用关闭后再回来。
+    #它只是起一个作用：把“启动阶段”和“关闭阶段”分开
+    yield
+    #关闭阶段,dispose用于应用关闭时释放数据库连接资源。
+    await engine.dispose()
+
+#创建 FastAPI 应用，并告诉它：“这个应用的生命周期由 lifespan 函数负责。”
+app = FastAPI(lifespan=lifespan)
+
+@app.post("/upload")
+async def upload_life(
+    file:UploadFile=File(...),
+    caption: str = Form(""),
+    session:AsyncSession= Depends(get_async_session)
+
+):
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 text_posts = {
     1: {"title": "New Post", "content": "Cool test post"},
     2: {"title": "Python Tip", "content": "Use list comprehensions for cleaner loops."},
@@ -15,6 +80,7 @@ text_posts = {
     9: {"title": "Question", "content": "What's the most underrated Python library you've ever used?"},
     10: {"title": "Mini Announcement", "content": "New video drops tomorrow—covering the weirdest Python features!"},
 }
+'''
 
 '''
 request(请求):Method(操作),Path(路径)，Body(真正内容)，Headers(身份认证)
@@ -28,7 +94,7 @@ PUT：更新数据。
 DELETE：删除数据。
 '''
 
-
+''''
 @app.get("/posts")
 def get_all_posts(limit:int | None = None):
     if limit:
@@ -43,12 +109,14 @@ def get_post(id:int)->PostResponse:
 
     return text_posts.get(id)
 
-'''
-果希望新增数据在重启后仍然存在，就需要使用 SQLite、MySQL 等数据库，而不能只使用 text_posts 这个内存字典。
-'''
+
+如果希望新增数据在重启后仍然存在，就需要使用 SQLite、MySQL 等数据库，而不能只使用 text_posts 这个内存字典。
+
 @app.post("/posts")
 def create_post(post:PostCreate)->PostResponse:
     new_post = {"title":post.title,"content":post.content}
     text_posts[max(text_posts.keys())+1] = new_post
     #return不是再次添加数据，而是告诉客户端创建的结果
     return new_post
+
+'''
--- src/fast_api/main.py
+++ src/fast_api/main.py
@@ -1,6 +1,6 @@
 import uvicorn
 
-
+#Uvicorn 是一个基于 ASGI（异步服务器网关接口）标准的、极速的 Python 异步 Web 服务器
 if __name__ == "__main__":
     uvicorn.run("fast_api.app:app",host="0.0.0.0",port=8000,reload=True)
 
